1b1381e118ca58abf83473ca03f9a206d4b88fa2
Mordenius Norrogem <mordenius@gmail.com>
2019-11-07 02:11:05 +0200

    chore(App) list of widgets and containers

--- lib/App.dart
+++ lib/App.dart
@@ -3,6 +3,32 @@ import 'package:flutter/material.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello'));
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: Scaffold(
+        appBar: AppBar(title: Text("Title ")),
+        body: Column(
+          children: [
+            Text('Question?'),
+            RaisedButton(
+              child: Text("Answer 1"),
+              onPressed: () {},
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: () {},
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: () {},
+            ),
+            RaisedButton(
+              child: Text("Answer 4"),
+              onPressed: () {},
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
