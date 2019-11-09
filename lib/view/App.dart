5a81a9e61a0d3c208f872cbbf4f7a2d2b2d4929a
Mordenius Norrogem <mordenius@gmail.com>
2019-11-09 03:08:54 +0200

    refactor(theme)

--- lib/view/App.dart
+++ lib/view/App.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-
-import './Router.dart';
+import './theme.dart';
+import './router.dart';
 
 class App extends StatelessWidget {
   final dynamic _domain;
@@ -11,15 +11,8 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(
-            fontFamily: "Quicksand",
-            appBarTheme: AppBarTheme(
-                textTheme: ThemeData.dark().textTheme.copyWith(
-                      title: TextStyle(
-                        fontFamily: "Quicksand",
-                        fontSize: 18,
-                      ),
-                    ))),
-        home: Router(this._domain));
+      theme: theme,
+      home: Router(this._domain),
+    );
   }
 }
