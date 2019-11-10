b52fff01db4f22d103a80790a518276ba1adf74d
Mordenius Norrogem <mordenius@gmail.com>
2019-11-10 23:49:36 +0200

    chore(deli_meals/screens/tabs) Bottom Navigation Bar

--- lib/deli_meals/view/screens/tabs.screen.dart
+++ lib/deli_meals/view/screens/tabs.screen.dart
@@ -5,30 +5,67 @@ class TabsScreen extends StatefulWidget {
   _TabsScreenState createState() => _TabsScreenState();
 }
 
+class Page {
+  final Widget page;
+  final String title;
+
+  Page(this.page, this.title);
+}
+
 class _TabsScreenState extends State<TabsScreen> {
+ final List<Page> _pages = [
+    Page(CategoriesScreen(), 'Categories'),
+    Page(FavoritesScreen(), 'Your Favorites')
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 2,
       initialIndex: 0,
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: theme.primaryColor,
-          title: Text('Meals'),
-          bottom: TabBar(
-            tabs: <Widget>[
-              Tab(icon: Icon(Icons.category), text: "Categories"),
-              Tab(icon: Icon(Icons.favorite), text: "Favorites")
-            ],
+          appBar: AppBar(
+            backgroundColor: theme.primaryColor,
+            title: Text(_pages[_selectedPageIndex].title),
+            bottom: TabBar(
+              tabs: <Widget>[
+                const Tab(icon: Icon(Icons.category), text: "Categories"),
+                const Tab(icon: Icon(Icons.favorite), text: "Favorites")
+              ],
+            ),
           ),
-        ),
-        body: TabBarView(
-          children: <Widget>[
-            CategoriesScreen(),
-            FavoritesScreen(),
-          ],
-        ),
-      ),
+          bottomNavigationBar: BottomNavigationBar(
+              onTap: _selectPage,
+              backgroundColor: theme.primaryColor,
+              unselectedItemColor: Colors.white70,
+              selectedItemColor: Colors.white,
+              currentIndex: _selectedPageIndex,
+              items: [
+               const BottomNavigationBarItem(
+                  icon: const Icon(Icons.category),
+                  title: const Text('Categories'),
+                ),
+               const BottomNavigationBarItem(
+                  icon: const Icon(Icons.star),
+                  title: const Text("Favorites"),
+                )
+              ]),
+          body: _pages[_selectedPageIndex].page
+          // body: TabBarView(
+          //   children: <Widget>[
+          //     CategoriesScreen(),
+          //     FavoritesScreen(),
+          //   ],
+          // ),
+          ),
     );
   }
 }
