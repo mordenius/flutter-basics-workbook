9f8d2d55784dfedcc67ffecdc0b72bbf6bcc096b
mordenius <mordenius@gmail.com>
2019-11-12 18:45:03 +0200

    frature(shop/view/widgets/order_item_tile) expanded order item

--- lib/shop/view/widgets/order_item_tile.dart
+++ lib/shop/view/widgets/order_item_tile.dart
@@ -1,9 +1,16 @@
 part of shop;
 
-class OrderItemTile extends StatelessWidget {
+class OrderItemTile extends StatefulWidget {
   final OrderItem _order;
 
   OrderItemTile(this._order);
+
+  @override
+  _OrderItemTileState createState() => _OrderItemTileState();
+}
+
+class _OrderItemTileState extends State<OrderItemTile> {
+  var _expanded = false;
 
   @override
   Widget build(BuildContext context) {
@@ -12,14 +19,43 @@ class OrderItemTile extends StatelessWidget {
       child: Column(
         children: <Widget>[
           ListTile(
-            title: Text('\$${_order.amount}'),
-            subtitle:
-                Text(DateFormat('dd MM yyyy hh:mm').format(_order.dateTime)),
+            title: Text('\$${widget._order.amount}'),
+            subtitle: Text(
+                DateFormat('dd MM yyyy hh:mm').format(widget._order.dateTime)),
             trailing: IconButton(
-              icon: Icon(Icons.expand_more),
-              onPressed: () {},
+              icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
           ),
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+              height: min(widget._order.products.length * 20.0 + 10, 180),
+              child: ListView(
+                children: widget._order.products
+                    .map(
+                      (CartItem cartItem) => Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            '${cartItem.title}',
+                            style: TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                          Text(
+                            '${cartItem.quantity}x \$${cartItem.price}',
+                            style: TextStyle(fontSize: 18, color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+            )
         ],
       ),
     );
