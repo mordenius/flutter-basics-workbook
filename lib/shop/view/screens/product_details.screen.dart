52c07f3fe7ae9e9fccabf1f5e39ce1c568a0cd87
mordenius <mordenius@gmail.com>
2019-11-12 18:01:27 +0200

    chore(shop/view/screen/product_details) preview

--- lib/shop/view/screens/product_details.screen.dart
+++ lib/shop/view/screens/product_details.screen.dart
@@ -10,6 +10,36 @@ class ProductDetailsScreen extends StatelessWidget {
     // Provider.of<Products>(context);
     return Scaffold(
       appBar: AppBar(title: Text(_product.title)),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(_product.imageUrl, fit: BoxFit.cover),
+            ),
+            SizedBox(height: 10),
+            Text(
+              '\$${_product.price}',
+              style: TextStyle(
+                color: Colors.grey,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0 ),
+                child: Text(
+                  _product.description,
+                  textAlign: TextAlign.center,
+                  softWrap: true,
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
