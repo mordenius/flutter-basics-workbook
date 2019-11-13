d91592c25d9257cb24ca0ad061d08bfdcfe02df9
Mordenius Norrogem <mordenius@gmail.com>
2019-11-13 22:37:08 +0200

    feature(shop/view/widgets/cart_product_item) confirmation of removing item from cart via popup dialog

--- lib/shop/view/widgets/cart_product_item.dart
+++ lib/shop/view/widgets/cart_product_item.dart
@@ -13,6 +13,30 @@ class CartProductItem extends StatelessWidget {
       onDismissed: (_) {
         _cart.removeById(_item.id);
       },
+      confirmDismiss: (direction) {
+        return showDialog(
+          context: context,
+          barrierDismissible: true,
+          builder: (ctx) => AlertDialog(
+            title: Text('Are you sure?'),
+            content: Text('Do you want to remove the item from the cart?'),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("No"),
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+              ),
+              FlatButton(
+                child: Text("Yes"),
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+              ),
+            ],
+          ),
+        );
+      },
       key: ValueKey(_item.id),
       background: Container(
         color: theme.errorColor,
