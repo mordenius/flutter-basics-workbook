f4af6302a92ea4c6cb132a9f13f9b310324cca2c
Mordenius Norrogem <mordenius@gmail.com>
2019-11-16 00:08:47 +0200

    fix(shop/providers/order) parse date and object field accessor []

--- lib/shop/providers/orders.provider.dart
+++ lib/shop/providers/orders.provider.dart
@@ -23,32 +23,29 @@ class Orders with ChangeNotifier {
 
   Future<void> fetchAndSetProducts() async {
     final url = "https://flutter-basics-workbook.firebaseio.com/orders.json";
-    try {
-      final Response response = await get(url);
-      final Map<String, dynamic> data = json.decode(response.body);
 
-      data.forEach((String id, dynamic item) {
-        _orders.add(
-          OrderItem(
-            id: id,
-            amount: item.amount,
-            dateTime: DateTime(item.dateTime),
-            products: item.product.map((cp) => {
-                  CartItem(
-                    id: cp.id,
-                    title: cp.title,
-                    quantity: int.tryParse(cp.quantity),
-                    price: double.parse(cp.price),
-                  )
-                }),
-          ),
-        );
-      });
-    } catch (error) {
-      throw error;
-    }
+    final Response response = await get(url);
+    final Map<String, dynamic> data = json.decode(response.body);
 
-    return Future.value();
+    data.forEach((String id, dynamic item) {
+      _orders.add(
+        OrderItem(
+          id: id,
+          amount: double.parse(item['amount']),
+          dateTime: DateTime.parse(item['dateTime']),
+          products: item['product']
+              .map((cp) => {
+                    CartItem(
+                      id: cp['id'],
+                      title: cp['title'],
+                      quantity: int.tryParse(cp['quantity']),
+                      price: double.parse(cp['price']),
+                    )
+                  })
+              .toList(),
+        ),
+      );
+    });
   }
 
   Future<void> addORder(List<CartItem> products, double total) async {
@@ -58,12 +55,14 @@ class Orders with ChangeNotifier {
     final String encodedProduct = json.encode({
       'amount': total,
       'dateTime': now.toIso8601String(),
-      'products': products.map((CartItem cp) => {
-            'id': cp.id,
-            'title': cp.title,
-            'quantity': cp.quantity,
-            'price': cp.price,
-          }).toList(),
+      'products': products
+          .map((CartItem cp) => {
+                'id': cp.id,
+                'title': cp.title,
+                'quantity': cp.quantity,
+                'price': cp.price,
+              })
+          .toList(),
     });
 
     String id;
