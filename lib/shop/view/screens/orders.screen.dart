6bc9bcfab859854a3f3a3dc61a28a130489b2576
Mordenius Norrogem <mordenius@gmail.com>
2019-11-16 00:29:06 +0200

    refactor(shop/view/screens/orders) future builder widget

--- lib/shop/view/screens/orders.screen.dart
+++ lib/shop/view/screens/orders.screen.dart
@@ -1,48 +1,41 @@
 part of shop;
 
-class OrdersScreen extends StatefulWidget {
+class OrdersScreen extends StatelessWidget {
   static final routeName = '/cart/orders';
 
   @override
-  _OrdersScreenState createState() => _OrdersScreenState();
-}
-
-class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
-
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) {
-      setState(() {
-        _isLoading = true;
-      });
-      return Provider.of<Orders>(context, listen: false).fetchAndSetProducts();
-    }).whenComplete(() {
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    Orders orders = Provider.of<Orders>(context);
+    Orders orders = Provider.of<Orders>(context, listen: false);
 
     return Scaffold(
       appBar: AppBar(
         title: Text("Orders"),
       ),
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: orders.orders.length,
-              itemBuilder: (ctx, int index) =>
-                  OrderItemTile(orders.orders[index]),
-            ),
+      body: FutureBuilder(
+          future: orders.fetchAndSetProducts(),
+          builder: (_ctx, dataSnapshot) {
+            if (dataSnapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            if (dataSnapshot.error != null) {
+              return Center(
+                child: Text('Shit happens', style: TextStyle(fontSize: 20)),
+              );
+            }
+
+            return Consumer<Orders>(
+              builder: (_ctx, Orders orders, Widget _widget) =>
+                  ListView.builder(
+                itemCount: orders.orders.length,
+                itemBuilder: (ctx, int index) =>
+                    OrderItemTile(orders.orders[index]),
+              ),
+            );
+          }),
     );
   }
 }
