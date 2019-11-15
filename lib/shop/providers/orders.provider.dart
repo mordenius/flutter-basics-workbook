2f0d669e3db9dc4fa2c2988506bc09d4034cb2df
Mordenius Norrogem <mordenius@gmail.com>
2019-11-15 23:53:47 +0200

    feature(shop/providers/orders) storing orders on server

--- lib/shop/providers/orders.provider.dart
+++ lib/shop/providers/orders.provider.dart
@@ -21,17 +21,78 @@ class Orders with ChangeNotifier {
     return _orders.toList();
   }
 
-  void addORder(List<CartItem> products, double total) {
+  Future<void> fetchAndSetProducts() async {
+    final url = "https://flutter-basics-workbook.firebaseio.com/orders.json";
+    try {
+      final Response response = await get(url);
+      final Map<String, dynamic> data = json.decode(response.body);
+
+      data.forEach((String id, dynamic item) {
+        _orders.add(
+          OrderItem(
+            id: id,
+            amount: item.amount,
+            dateTime: DateTime(item.dateTime),
+            products: item.product.map((cp) => {
+                  CartItem(
+                    id: cp.id,
+                    title: cp.title,
+                    quantity: int.tryParse(cp.quantity),
+                    price: double.parse(cp.price),
+                  )
+                }),
+          ),
+        );
+      });
+    } catch (error) {
+      throw error;
+    }
+
+    return Future.value();
+  }
+
+  Future<void> addORder(List<CartItem> products, double total) async {
+    final url = "https://flutter-basics-workbook.firebaseio.com/orders.json";
     var now = DateTime.now();
+
+    final String encodedProduct = json.encode({
+      'amount': total,
+      'dateTime': now.toIso8601String(),
+      'products': products.map((CartItem cp) => {
+            'id': cp.id,
+            'title': cp.title,
+            'quantity': cp.quantity,
+            'price': cp.price,
+          }).toList(),
+    });
+
+    String id;
+
+    try {
+      final Response response = await post(
+        url,
+        body: encodedProduct,
+      );
+
+      final data = json.decode(response.body);
+      id = data['name'];
+    } catch (error) {
+      print(error.toString());
+      throw error;
+    }
+
     _orders.insert(
       0,
       OrderItem(
-        id: now.toString(),
+        id: id,
         amount: total,
         dateTime: now,
         products: products,
       ),
     );
+
     notifyListeners();
+
+    return Future.value();
   }
 }
