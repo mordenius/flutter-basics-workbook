0368dc466c8d2a447e405de0311837a9472b59f6
Mordenius Norrogem <mordenius@gmail.com>
2019-11-16 17:33:38 +0200

    chore(shop/view/screens/user_products) filter products by user

--- lib/shop/view/screens/user_products.screen.dart
+++ lib/shop/view/screens/user_products.screen.dart
@@ -5,38 +5,49 @@ class UserProductsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Products products = Provider.of<Products>(context);
-
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Your products'),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () {
-                Navigator.of(context).pushNamed(EditProductScreen.routeName);
-              },
-            ),
-          ],
-        ),
-        body: RefreshIndicator(
-          onRefresh: () {
-            return products.fetchAndSetProducts();
-          },
-          child: Padding(
-            padding: EdgeInsets.all(8),
-            child: ListView.builder(
-              itemCount: products.items.length,
-              itemBuilder: (_, int index) => Column(
-                children: <Widget>[
-                  UserProduct(products.items[index]),
-                  Divider(
-                    height: 10,
+      appBar: AppBar(
+        title: Text('Your products'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.of(context).pushNamed(EditProductScreen.routeName);
+            },
+          ),
+        ],
+      ),
+      body: FutureBuilder(
+        future: Provider.of<Products>(context).fetchAndSetProducts(true),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+
+          return RefreshIndicator(
+            onRefresh: () {
+              return Provider.of<Products>(ctx, listen: false)
+                  .fetchAndSetProducts(true);
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8),
+              child: Consumer<Products>(
+                builder: (_ctx, products, _widget) => ListView.builder(
+                  itemCount: products.items.length,
+                  itemBuilder: (_, int index) => Column(
+                    children: <Widget>[
+                      UserProduct(products.items[index]),
+                      Divider(
+                        height: 10,
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
-          ),
-        ));
+          );
+        },
+      ),
+    );
   }
 }
