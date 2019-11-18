cd6567629b7a82fb3e6fa8bc4fc9ac4ace2c7348
Mordenius Norrogem <mordenius@gmail.com>
2019-11-18 23:39:09 +0200

    chore(great_places/screens/place_detail_screen) place detail page

--- lib/great_places/screens/place_detail_screen.dart
+++ lib/great_places/screens/place_detail_screen.dart
@@ -2,14 +2,61 @@ import 'package:flutter/material.dart';
 
 import './../models/place.dart';
 
+import './map_screen.dart';
+
 class PlaceDetailScreen extends StatelessWidget {
   static final String routeName = '/great-places/place-detail';
-  
+
   @override
   Widget build(BuildContext context) {
-    final Place place = ModalRoute.of(context).settings.arguments; 
+    final Place place = ModalRoute.of(context).settings.arguments;
     return Scaffold(
-      appBar: AppBar(title: Text(place.title)),
-    );
+        appBar: AppBar(title: Text(place.title)),
+        body: Column(
+          children: <Widget>[
+            Container(
+              height: 250,
+              width: double.infinity,
+              child: Image.file(
+                place.image,
+                fit: BoxFit.cover,
+                width: double.infinity,
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                place.location.address,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.grey,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            FlatButton(
+              child: Text('View on Map'),
+              textColor: Theme.of(context).primaryColor,
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    fullscreenDialog: true,
+                    builder: (ctx) {
+                      return MapScreen(
+                        initialLocation: place.location,
+                      );
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ));
   }
 }
