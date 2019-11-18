b81005ff1dc3236615835a3d8ea45eac8ebb9b11
Mordenius Norrogem <mordenius@gmail.com>
2019-11-18 03:56:22 +0200

    chore(great_places/widgets/location_input) get user current location

--- lib/great_places/widgets/location_input.dart
+++ lib/great_places/widgets/location_input.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 class LocationInput extends StatefulWidget {
   final Function onSelectLocation;
@@ -11,6 +12,12 @@ class LocationInput extends StatefulWidget {
 
 class _LocationInputState extends State<LocationInput> {
   String _previewImageUrl;
+
+  Future<void> _getUserCurrentLocation() async {
+    final locData = await Location().getLocation();
+    print(locData.latitude);
+    print(locData.longitude);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,7 +45,7 @@ class _LocationInputState extends State<LocationInput> {
                 icon: Icon(Icons.location_on),
                 label: Text('Current Location'),
                 textColor: Theme.of(context).primaryColor,
-                onPressed: () {}),
+                onPressed: _getUserCurrentLocation),
             FlatButton.icon(
                 icon: Icon(Icons.map),
                 label: Text('Select on Map'),
