8cad7b00365f06ecb399fbf69bbc9b47eecfc6a6
Mordenius Norrogem <mordenius@gmail.com>
2019-11-20 23:55:34 +0200

    fix(number_trivia/domain/repository/number_trivia) get random without params

--- lib/number_trivia/domain/repositories/number_trivia_repository.dart
+++ lib/number_trivia/domain/repositories/number_trivia_repository.dart
@@ -4,5 +4,5 @@ import './../entities/number_trivia.dart';
 
 abstract class NumberTriviaRepository {
   Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number);
-  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia(int number);
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia();
 }
