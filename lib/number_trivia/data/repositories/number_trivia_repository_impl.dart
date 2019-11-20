f119879c32971dcf0a4e010a62d83f54255a3885
Mordenius Norrogem <mordenius@gmail.com>
2019-11-21 01:47:15 +0200

    feature(number_trivia/data/repositories/number_trivia) fetch data from remote data source when device is online

---
+++ lib/number_trivia/data/repositories/number_trivia_repository_impl.dart
@@ -0,0 +1,32 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter/foundation.dart';
+
+import './../../../core/platform/network_info.dart';
+import './../../../core/exceptions/failures.dart';
+import './../../domain/entities/number_trivia.dart';
+import './../../domain/repositories/number_trivia_repository.dart';
+import './../datasources/number_trivia_local_data_source.dart';
+import './../datasources/number_trivia_remote_data_source.dart';
+
+class NumberTriviaRepositoryImpl implements NumberTriviaRepository {
+  final NumberTriviaRemoteDataSource remoteDataSource;
+  final NumberTriviaLocalDataSource localeDataSource;
+  final NetworkInfo networkInfo;
+
+  NumberTriviaRepositoryImpl(
+      {@required this.remoteDataSource,
+      @required this.localeDataSource,
+      @required this.networkInfo});
+
+  @override
+  Future<Either<Failure, NumberTrivia>> getConcreteNumberTrivia(int number) async {
+    networkInfo.isConnected;
+    return Right(await remoteDataSource.getConcreteNumberTrivia(number));
+  }
+
+  @override
+  Future<Either<Failure, NumberTrivia>> getRandomNumberTrivia() {
+    // TODO: implement getRandomNumberTrivia
+    return null;
+  }
+}
