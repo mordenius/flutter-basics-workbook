f09c2b94a251cca27538fbc48c260abff6082878
Mordenius Norrogem <mordenius@gmail.com>
2019-11-22 04:06:09 +0200

    spec(number_trivia/data/datasources/local) get cached trivia

---
+++ test/number_trivia/data/datasources/number_trivia_local_data_source_test.dart
@@ -0,0 +1,51 @@
+import 'dart:convert';
+
+import 'package:flutter_basics_workbook/core/exceptions/exceptions.dart';
+import 'package:flutter_basics_workbook/number_trivia/data/models/number_trivia_model.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:matcher/matcher.dart';
+
+import 'package:flutter_basics_workbook/number_trivia/data/datasources/number_trivia_local_data_source.dart';
+
+import '../../../fixtures/fixture_reader.dart';
+
+class MockSharedPreferences extends Mock implements SharedPreferences {}
+
+void main() {
+  NumberTriviaLocalDataSourceImpl dataSource;
+  MockSharedPreferences mockSharedPreferences;
+
+  setUp(() {
+    mockSharedPreferences = MockSharedPreferences();
+    dataSource = NumberTriviaLocalDataSourceImpl(
+        sharedPreferences: mockSharedPreferences);
+  });
+
+  group('getLastNumberTrivia', () {
+    final tNumberTriviaModel =
+        NumberTriviaModel.fromJson(json.decode(fixture('trivia_cached.json')));
+
+    test(
+        'should return NumberTrivia from SharedPreferences when there is one in the cache',
+        () async {
+      when(mockSharedPreferences.getString(any))
+          .thenReturn(fixture('trivia_cached.json'));
+
+      final result = await dataSource.getLastNumberTrivia();
+
+      verify(mockSharedPreferences.getString('CACHED_NUMBER_TRIVIA'));
+      expect(result, equals(tNumberTriviaModel));
+    });
+
+    test('should throw a CacheException when there is not a cached value',
+        () async {
+      when(mockSharedPreferences.getString(any)).thenReturn(null);
+
+      final call = dataSource.getLastNumberTrivia;
+
+      expect(() => call(), throwsA(TypeMatcher<CacheException>()));
+    });
+  });
+}
