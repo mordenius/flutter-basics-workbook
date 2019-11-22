9f57491ae0d0a3ad8951768b3b07f283b3e88049
Mordenius Norrogem <mordenius@gmail.com>
2019-11-22 04:54:31 +0200

    feature(number_trivia/data/datasources/remote) get concrete number trivia

--- lib/number_trivia/data/datasources/number_trivia_remote_data_source.dart
+++ lib/number_trivia/data/datasources/number_trivia_remote_data_source.dart
@@ -1,6 +1,38 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_basics_workbook/core/exceptions/exceptions.dart';
+import 'package:flutter_basics_workbook/number_trivia/data/models/number_trivia_model.dart';
+import 'package:http/http.dart' as http;
+
 import './../../domain/entities/number_trivia.dart';
 
 abstract class NumberTriviaRemoteDataSource {
   Future<NumberTrivia> getConcreteNumberTrivia(int number);
   Future<NumberTrivia> getRandomNumberTrivia();
+}
+
+class NumberTriviaRemoteDataSourceImpl implements NumberTriviaRemoteDataSource {
+  final http.Client client;
+
+  NumberTriviaRemoteDataSourceImpl({@required this.client});
+
+  @override
+  Future<NumberTrivia> getConcreteNumberTrivia(int number) async {
+    final http.Response response = await client.get(
+        'http://numbertrivia.com/$number',
+        headers: {'Content-Type': 'application/json'});
+
+    if (response.statusCode == 200) {
+      return NumberTriviaModel.fromJson(json.decode(response.body));
+    }
+
+    throw ServerException();
+  }
+
+  @override
+  Future<NumberTrivia> getRandomNumberTrivia() {
+    // TODO: implement getRandomNumberTrivia
+    return null;
+  }
 }
