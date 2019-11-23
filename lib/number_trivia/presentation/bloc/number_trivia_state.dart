826d5ffdd2c1e944fcb58527980d48b70dc94fc7
mordenius <mordenius@gmail.com>
2019-11-23 19:35:55 +0200

    chore(number_trivia/presentation/bloc/number_trivia_state) append number trivia states

--- lib/number_trivia/presentation/bloc/number_trivia_state.dart
+++ lib/number_trivia/presentation/bloc/number_trivia_state.dart
@@ -1,10 +1,24 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter/foundation.dart';
+
+import './../../domain/entities/number_trivia.dart';
 
 abstract class NumberTriviaState extends Equatable {
-  const NumberTriviaState();
+  NumberTriviaState([List props = const <dynamic>[]]) : super(props);
 }
 
-class InitialNumberTriviaState extends NumberTriviaState {
-  @override
-  List<Object> get props => [];
+class InitialNumberTriviaState extends NumberTriviaState {}
+
+class LoadingNumberTriviaState extends NumberTriviaState {}
+
+class LoadedNumberTriviaState extends NumberTriviaState {
+  final NumberTrivia trivia;
+
+  LoadedNumberTriviaState({@required this.trivia}) : super([trivia]);
+}
+
+class ErrorNumberTriviaState extends NumberTriviaState {
+  final String message;
+
+  ErrorNumberTriviaState({@required this.message}) : super([message]);
 }
