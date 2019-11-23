58508ae3fd906574cf5afd87b0fdae3aceb96b19
mordenius <mordenius@gmail.com>
2019-11-23 19:26:55 +0200

    chore(number_trivia/presentation/bloc/number_trivia_event) append  events get concrete and random number

--- lib/number_trivia/presentation/bloc/number_trivia_event.dart
+++ lib/number_trivia/presentation/bloc/number_trivia_event.dart
@@ -3,3 +3,13 @@ import 'package:equatable/equatable.dart';
 abstract class NumberTriviaEvent extends Equatable {
   NumberTriviaEvent([List props = const <dynamic>[]]) : super(props);
 }
+
+class GetTriviaForConcreteNumber extends NumberTriviaEvent {
+  final String numberString;
+
+  GetTriviaForConcreteNumber(this.numberString) : super([numberString]);
+}
+
+class GetTriviaForRandomNumber extends NumberTriviaEvent {
+
+}
