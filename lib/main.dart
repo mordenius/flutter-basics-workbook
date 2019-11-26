5eefcdf147e32e912d1db489c8b5a706d6d5e3d3
Mordenius Norrogem <mordenius@gmail.com>
2019-11-27 01:55:41 +0200

    refactor(quiz) remove old

--- lib/main.dart
+++ lib/main.dart
@@ -2,45 +2,25 @@ import 'package:flutter_config/flutter_config.dart';
 import 'package:flutter/material.dart';
 import './great_places/great_places.dart';
 
-import './domain/quiz.dart' as quizlib;
 import './domain/expenses.dart' as expenseslib;
 
+import './quiz/quiz.dart';
 import './number_trivia/number_trivia.dart' as nt;
 
 // import './view/App.dart';
 
-// var quizlibLogic = quizlib.QuizLogic([
-//   quizlib.Question(
-//     text: "Question one?",
-//     answers: [
-//       quizlib.Answer(text: "One", score: 1),
-//       quizlib.Answer(text: "Two", score: 2),
-//       quizlib.Answer(text: "Three", score: 3),
-//       quizlib.Answer(text: "Four", score: 4),
-//     ],
-//   ),
-//   quizlib.Question(
-//     text: "Question two?",
-//     answers: [
-//       quizlib.Answer(text: "One", score: 4),
-//       quizlib.Answer(text: "Two", score: 3),
-//       quizlib.Answer(text: "Three", score: 2),
-//       quizlib.Answer(text: "Four", score: 1),
-//     ],
-//   )
-// ]);
-
 // var expensesLogic = expenseslib.ExpensesLogic();
 
 // class Domain {
-//   final quizlib.QuizLogic quiz = quizlibLogic;
 //   final expenseslib.ExpensesLogic expenses = expensesLogic;
 // }
 
 Future<void> main() async {
   // await FlutterConfig.loadEnvVariables();
-  
+
   // runApp(GreatPlacesApp());
 
-  nt.main();
+  // nt.main();
+
+  runApp(QuizApp());
 }
