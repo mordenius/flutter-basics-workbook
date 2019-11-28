9a0d0bf6d72b57d21e1709c2805bd1f3e113264b
Mordenius Norrogem <mordenius@gmail.com>
2019-11-28 23:51:11 +0200

    refactor(expenses/screen) modal bottom

--- lib/expenses_refactor/screens/expenses_screen.dart
+++ lib/expenses_refactor/screens/expenses_screen.dart
@@ -4,14 +4,37 @@ import 'package:provider/provider.dart';
 import './../providers/expenses.dart';
 
 import './../widgets/transactions_list.dart';
+import './../widgets/append_transaction.dart';
 
 class ExpensesScreen extends StatelessWidget {
+  _startAddNew(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return GestureDetector(
+            child: AppendTransaction(null),
+            behavior: HitTestBehavior.opaque,
+            onTap: () {},
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Expenses'),
-      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            _startAddNew(context);
+          }),
+      appBar: AppBar(title: Text("Expenses "), actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () {
+            _startAddNew(context);
+          },
+        )
+      ]),
       body: Consumer<Expenses>(
         builder: (ctx, expenses, _) {
           return TransactionsList(
